d031aee5779fdeaa19f018f3bb17e5942879f047
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-12 01:45:38 +0500

    telega3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+var botClient = new TelegramBotClient ("5215566052:AAFgF40V0IVpe4UtClu0nvebwY-rLQHtLQE");
+
+var me = await botClient.GetMeAsync();
+
+while (true)
+{
+    var updates = await botClient.GetUpdatesAsync();
+    for (int i = 0; i < updates.Count(); i++)
+    {
+        switch (updates[i].Type)
+        {
+            case Telegram.Bot.Types.Enums.UpdateType.Message: {
+                MessageHandle(updates[i].Message!);
+                updates = await botClient.GetUpdatesAsync(updates[^1].Id + 1);
+                break;
+            }
+        }
+    }
+}
+async void MessageHandle (Message message){
+    await botClient!.SendTextMessageAsync(message.Chat.Id, $"{message.From.Username} Ты мне писал {message.Text}?");
+}
+
