d9f02a161308e5fd7a6e45201df54b25da8cbe19
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-12 02:11:56 +0500

    telega6

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 
-var botClient = new TelegramBotClient ("5215566052:AAFgF40V0IVpe4UtClu0nvebwY-rLQHtLQE");
+var botClient = new TelegramBotClient("5215566052:AAFgF40V0IVpe4UtClu0nvebwY-rLQHtLQE");
 
 var me = await botClient.GetMeAsync();
 
@@ -12,22 +12,28 @@ while (true)
     {
         switch (updates[i].Type)
         {
-            case Telegram.Bot.Types.Enums.UpdateType.Message: {
-                MessageHandle(updates[i].Message!);
-                updates = await botClient.GetUpdatesAsync(updates[^1].Id + 1);
-                break;
-            }
+            case Telegram.Bot.Types.Enums.UpdateType.Message:
+                {
+                    MessageHandle(updates[i].Message!);
+                    updates = await botClient.GetUpdatesAsync(updates[^1].Id + 1);
+                    break;
+                }
 
-            
+
         }
     }
 }
-async void MessageHandle (Message message){
-    if (message.Text == "Привет!"){
+async void MessageHandle(Message message)
+{
+    if (message.Text == "Привет"){
         await botClient!.SendTextMessageAsync(message.Chat.Id, "Ухты, а ты вежливый!");
-    }
+    }   
+    int number = Convert.ToInt32(message.Text);
+    
+    int square = number * number;
 
-
-    await botClient!.SendTextMessageAsync(message.Chat.Id, $"{message.From.Username} Ты мне писал {message.Text}?");
+    //await botClient!.SendTextMessageAsync(message.Chat.Id, $"{message.From.Username} Ты мне писал {message.Text}?");
+    //await botClient!.SendTextMessageAsync(message.Chat.Id, $"{message.Text}");
+    //await botClient!.SendTextMessageAsync(message.Chat.Id, $"{square}");
 }
 
